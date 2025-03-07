65788bd1643749df29b74dce2aae44f73741d9b6
wingchichan <w.chi.chan@gmail.com>
2025-03-07 20:13:06 +0000

    displaying comments

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -2,13 +2,45 @@
 // need to see comments related to this post
 import pg from "pg";
 import Image from "next/image";
+import { comment } from "postcss";
 
 export default async function Page({ params }) {
   const slug = await params;
   const db = new pg.Pool({ connectionString: process.env.DB_CONN });
-  const post = await db.query(`SELECT * FROM posts WHERE id = ${slug.id}`);
-  // need [0] to display the singular
+
+  //   const post = await db.query(`SELECT * FROM posts WHERE id = ${slug.id}`);
+  //   // need [0] to display the singular
+  //   const singlePost = post.rows[0];
+
+  //   const comments = await db.query(
+  //     `SELECT * FROM comments WHERE post_id = ${slug.id}`
+  //   );
+  //   console.log(comments);
+  //   const singleComment = comments.rows[0];
+  //   console.log(singleComment);
+
+  //   const post = await db.query(`SELECT posts.*, ARRAY_AGG(comments.*) AS comments
+  //   FROM posts
+  //   INNER JOIN comments on comments.post_id = posts.id
+  //   GROUP BY comments.post_id, posts.id`);
+  //   console.log(post);
+
+  //   const postWithComms = post.rows[0];
+  //   console.log(postWithComms);
+
+  const post = await db.query(`SELECT posts.*, ARRAY_AGG(json_build_object(
+  'id', comments.id,
+  'content', comments.content,
+  'post_id', comments.post_id
+)) AS comments
+FROM posts
+INNER JOIN comments on comments.post_id = posts.id
+GROUP BY comments.post_id, posts.id`);
+
   const singlePost = post.rows[0];
+  console.log(singlePost);
+  const comments = singlePost.comments[0];
+  console.log(comments);
 
   return (
     <div>
@@ -20,6 +52,9 @@ export default async function Page({ params }) {
         src={singlePost.image}
       />
       <p>{singlePost.content}</p>
+      <section>
+        <p>{comments.content}</p>
+      </section>
     </div>
   );
 }
